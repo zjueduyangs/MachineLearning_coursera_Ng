cd4c0f8a46363f5859195afc2297f6af2e48d9cb
zjueduyangs <zjueduys@foxmail.com>
2016-12-14 14:07:48 +0800

    accomplish linear regression with one variable

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -6,8 +6,8 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
+% You need to return the following variables correctly
+J = sum((X*theta-y).^2)/(2*m);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
