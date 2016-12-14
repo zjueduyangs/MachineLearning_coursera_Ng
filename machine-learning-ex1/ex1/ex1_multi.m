c2ac6fe88b43ed862e70b8c3842157b765f1eba2
zjueduyangs <zjueduys@foxmail.com>
2016-12-14 17:20:43 +0800

    accomplish normal equations

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -82,8 +82,8 @@ X = [ones(m, 1) X];
 fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
-alpha = 0.1;
-num_iters = 50;
+alpha = 0.01;
+num_iters = 400;
 
 % Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
@@ -150,7 +150,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1 1650 3]*theta; % You should change this
 
 
 % ============================================================
